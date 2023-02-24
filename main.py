96fbd2159ef985c9bd2c0148dd5fc2e235e5365e
Evgeniy Kudryavtsev <ekv222@gmail.com>
2023-02-24 21:53:45 +0800

    полностью переделал...

--- main.py
+++ main.py
@@ -1,70 +1,26 @@
-class Files:
+class MergingFiles:
     """"""
 
-    def __init__(self, file_name: str):
-        self.file_name = file_name
-        self.numb_rows = self.__get_numb_rows()
-        self.data = self.__get_data_file()
+    @staticmethod
+    def create_merge_file(sorted_file_name, *args):
+        """"""
 
-    def __get_data_file(self):
-        with open(self.file_name, encoding='UTF-8') as f:
-            self.data = f.read()
-        return self.data
-
-    def __get_numb_rows(self):
-        with open(self.file_name, encoding='UTF-8') as f:
-            self.numb_rows = len(f.readlines())
-        return self.numb_rows
-
-    def __lt__(self, other):
-        if not isinstance(other, Files):
-            return "Not a Files!"
-        return self.numb_rows < other.numb_rows
-
-    def __gt__(self, other):
-        if not isinstance(other, Files):
-            return "Not a Files!"
-        return self.numb_rows > other.numb_rows
-
-    def __eq__(self, other):
-        if not isinstance(other, Files):
-            return "Not a Files!"
-        return self.numb_rows == other.numb_rows
-
-
-def create_dict_files(list_file: list):
-    """"""
-
-    dict_files = {}
-    for file in list_file:
-        if not isinstance(file, Files):
-            return print('Список "list_files" должен состоять из элементов класса "Files".')
-
-    for file in list_file:
-        attr_files = {}
-        attr_files['numb_rows'] = file.numb_rows
-        attr_files['data'] = file.data
-        dict_files[file.file_name] = attr_files
-
-    return dict_files
-
-
-def create_sorted_file(new_file_name, dict_file: dict):
-    """"""
-
-    with open(new_file_name, 'w', encoding='UTf-8') as f:
-        for key, value in dict_file.items():
-            f.write(key + '\n')
-            f.write(str(value['numb_rows']) + '\n')
-            f.write(value['data'] + '\n')
+        dict_name_and_numb = {}
+        for file in args:
+            with open(file, encoding='UTF-8') as f:
+                dict_name_and_numb[file] = len(f.readlines())
+        sort_files_dict = dict(sorted(dict_name_and_numb.items(), key=lambda i: i[1]))
+        with open(sorted_file_name, 'w', encoding='UTF-8') as f_new:
+            for name, numb in sort_files_dict.items():
+                with open(name, encoding='UTF-8') as f:
+                    data = f.read()
+                    f_new.write(name + '\n')
+                    f_new.write(str(numb) + '\n')
+                    f_new.write(data + '\n')
 
 
 def main():
-    file_1 = Files('files/1.txt')
-    file_2 = Files('files/2.txt')
-    file_3 = Files('files/3.txt')
-    dict_files = create_dict_files(sorted([file_1, file_2, file_3]))
-    create_sorted_file('files/sorted.txt', dict_files)
+    MergingFiles().create_merge_file('files/sorted.txt', 'files/1.txt', 'files/2.txt', 'files/3.txt')
 
 
 if __name__ == '__main__':
